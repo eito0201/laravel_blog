255710ccfe9c7cb19e55fbb56a40679f47b0b8cc
eito0201 <eito.k.8888@gmail.com>
2020-12-05 13:12:56 +0900

    create posts/index view

--- backend/app/Http/Controllers/PostsController.php
+++ backend/app/Http/Controllers/PostsController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostsController extends Controller
 {
     public function index()
     {
-        return "hello";
+        // 投稿日時の新しい順に投稿一覧を取得
+        $posts = Post::latest()->get();
+        return view('posts.index', ['posts' => $posts]);
     }
 }
