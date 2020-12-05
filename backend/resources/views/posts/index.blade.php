9e28027a2cdbc7dbe9e494db0a6db6ede3a83049
eito0201 <eito.k.8888@gmail.com>
2020-12-05 14:56:15 +0900

    create layouts/default view

--- backend/resources/views/posts/index.blade.php
+++ backend/resources/views/posts/index.blade.php
@@ -1,21 +1,12 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="/css/styles.css">
-  <title>Blog Posts</title>
-</head>
-<body>
-  <div class="container">
-    <h1>Blog Posts</h1>
-    <ul>
-      @forelse ($posts as $post)
-      <li><a href="{{ action('PostsController@show', $post) }}">{{ $post->title }}</a></li>
-      @empty
-      <li>No posts yet</li>
-      @endforelse
-    </ul>  
-  </div>
-</body>
-</html>
+@extends('layouts/default')
+@section('title', 'Blog Posts')
+@section('content')
+<h1>Blog Posts</h1>
+<ul>
+  @forelse ($posts as $post)
+  <li><a href="{{ action('PostsController@show', $post) }}">{{ $post->title }}</a></li>
+  @empty
+  <li>No posts yet</li>
+  @endforelse
+</ul>  
+@endsection
